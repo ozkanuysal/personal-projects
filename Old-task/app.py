86312f92c9bf082be7d376a9b1663ceae065a8cd
ozkanvitamu <ozkan@vitamu.com>
2024-04-16 11:54:19 +0300

    remove whitespace from fastapi

--- Old-task/app.py
+++ Old-task/app.py
@@ -236,6 +236,4 @@ def ReadFromVdfPrediction(date):
     return result
 
 if __name__ == "__main__":
-    # app.run(debug=True)
-    # app.run(debug=True, host='0.0.0.0')
     app.run(host="0.0.0.0",port = 8080)
