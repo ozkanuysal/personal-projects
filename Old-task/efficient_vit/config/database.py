b6581ca64b863783f81427372f5b0c6e233e311d
ozkanuysal <ozkan.uysal.2009@hotmail.com>
2024-08-09 12:14:17 +0300

    config section init files

---
+++ Old-task/efficient_vit/config/database.py
@@ -0,0 +1,18 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from config.settings import Settings
+
+SQLALCHEMY_DATABASE_URL = Settings.DATABASE_URL
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL, echo=True)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
