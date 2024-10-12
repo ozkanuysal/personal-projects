bb33cd2331d9b24602b12d6383a7fd465c5f6599
ozkanuysal <ozkan.uysal.2009@hotmail.com>
2024-10-12 20:44:29 +0300

    add auth schemas file

---
+++ Old-task/crud_project/src/auth/schemas.py
@@ -0,0 +1,55 @@
+import uuid
+from datetime import datetime
+from typing import List
+
+from pydantic import BaseModel, Field
+
+class UserCreateModel(BaseModel):
+    first_name: str = Field(max_length=25)
+    last_name: str = Field(max_length=25)
+    username: str = Field(max_length=8)
+    email: str = Field(max_length=40)
+    password: str = Field(min_length=6)
+
+    model_config = {
+        "json_schema_extra": {
+            "example": {
+                "first_name": "John",
+                "last_name": "Doe",
+                "username": "johndoe",
+                "email": "johndoe123@co.com",
+                "password": "testpass123",
+            }
+        }
+    }
+
+
+class UserModel(BaseModel):
+    uid: uuid.UUID
+    username: str
+    email: str
+    first_name: str
+    last_name: str
+    is_verified: bool
+    password_hash: str = Field(exclude=True)
+    created_at: datetime
+    update_at: datetime
+
+
+
+class UserLoginModel(BaseModel):
+    email: str = Field(max_length=40)
+    password: str = Field(min_length=6)
+
+
+class EmailModel(BaseModel):
+    addresses : List[str]
+
+
+class PasswordResetRequestModel(BaseModel):
+    email: str
+
+
+class PasswordResetConfirmModel(BaseModel):
+    new_password: str
+    confirm_new_password: str
