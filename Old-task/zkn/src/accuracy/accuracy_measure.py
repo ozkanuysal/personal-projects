61d31e1237d2beabae7a3b15ba6f932eceb803e1
Özkan Uysal <61411331+ozkanuysal@users.noreply.github.com>
2024-11-22 12:27:12 +0300

    init acc metric

---
+++ Old-task/zkn/src/accuracy/accuracy_measure.py
@@ -0,0 +1,153 @@
+
+import json, os
+import numpy as np
+from glob import glob 
+import ezkl
+import matplotlib.pyplot as plt
+from tqdm import tqdm
+
+def proof_file_to_io(prooffilename: str, scale: int) -> (np.array, np.array):
+    """
+    This will take a proof file and return the input and output values as numpy arrays.
+    """
+
+    with open(prooffilename, 'r') as f:
+        proof = json.load(f)
+    
+    if 'instances' not in proof and '.proof' not in prooffilename:
+        print("Looks like you might be using a witness file, I'll try loading a witness file instead")
+        proof_input = proof['inputs'][0]
+        proof_output = proof['outputs'][0]
+    else:
+        proof_instances = proof['instances']
+        proof_input = proof_instances[0]
+        proof_output = proof_instances[1]
+
+    proof_input = np.array([ezkl.vecu64_to_float(byte_array, scale) for byte_array in proof_input]).flatten()
+    proof_output = np.array([ezkl.vecu64_to_float(byte_array, scale) for byte_array in proof_output]).flatten()
+
+    return proof_input, proof_output
+
+def input_file_to_io(inputfilename: str) -> (np.array, np.array):
+    """This will take an input.json file and return the input and output values."""
+    with open(inputfilename, 'r') as f:
+        input_file = json.load(f)
+    return np.array(input_file['input_data']).flatten(), np.array(input_file['output_data']).flatten() if 'output_data' in input_file else None
+
+
+def calculate_differences(real_values: np.array, comparison_values: np.array, sigma: int = 4):
+    """
+    This will look at the differences between the real values and the output values based on MSE (mean squared error). It will return overall performance and can flag any points that are 4 standard deviations away from the mean std of error.
+
+    :param real_values: the real values
+    :param comparison_values: the output values
+    :param sigma: the number of standard deviations to use as a cutoff
+
+    :return: (data_mse, high_sigma_points, std_of_fsp)
+    """
+    se = np.square(real_values - comparison_values)
+    data_mse = np.mean(se)
+    high_sigma_points = se > sigma*data_mse
+    std_of_fsp = se[high_sigma_points] / data_mse
+
+    return data_mse, len(high_sigma_points), se, high_sigma_points, std_of_fsp
+
+
+def accuracy_analysis(real_values: np.array, comparison_values: np.array, plot_statistical_checks:bool = True, sigma: int = 4, fig_save_path: str | None = None ):
+    """
+    This will look at the differences between the real values and the output values based on MSE (mean squared error). It will return overall performance and can will show analysis tools to examine errors that deviate significantly. 
+
+    :param real_values: the original values
+    :param comparison_values: the proof values
+    :plot_statistical_checks: whether to plot the statistical checks (turn off for fast calculation of data_mse)
+    :param sigma: the number of standard deviations to use as a cutoff
+
+    :return: (data_mse, max_error, max_error_as_a_percent_of_range)
+
+    """
+
+    data_mse, high_sigma_count, se, high_sigma_points, std_of_fsp = calculate_differences(real_values, comparison_values, sigma=sigma)
+
+    max_error = np.sqrt(np.max(se))
+
+    max_error_as_a_percent_of_range = max_error / (np.max(real_values) - np.min(real_values))
+
+    data_mse_as_a_percent_of_range = data_mse / (np.max(real_values) - np.min(real_values))
+
+    median_value_of_high_sigma = np.median(real_values[high_sigma_points])
+
+    print(f"Data MSE: {data_mse}")
+    print(f"Number of points with error > {sigma} standard deviations: {high_sigma_count}")
+    print(f"Max error as a percent of range: {max_error_as_a_percent_of_range*100}%")
+    print("Run with plot_statistical_checks = True for detailed analysis")
+
+    if plot_statistical_checks:
+        plt.rcParams['axes.formatter.useoffset'] = False
+        plt.rcParams['axes.formatter.limits'] = (-3, 3)
+
+        fig, axes = plt.subplots(2,2)
+
+        axes[0][0].scatter(real_values, comparison_values, s=3)
+        axes[0][0].scatter(real_values[high_sigma_points], comparison_values[high_sigma_points], s=3, c='r', label=f'{sigma}$\sigma$ points')
+        plt.legend()
+        axes[0][0].set_xlabel('Original Values')
+        axes[0][0].set_ylabel('Proof Values')
+        axes[0][0].set_title('Actual Value Comparison')
+        axes[0][0].legend()
+
+        axes[1][0].scatter(real_values, se, s=4)
+        axes[1][0].scatter(real_values[high_sigma_points], se[high_sigma_points], s=4, c = 'r')
+        axes[1][0].set_xlabel('Original Values')
+        axes[1][0].set_ylabel('Squared Error (se)')
+        axes[1][0].set_title('Error Homoscedasticity')
+
+        counts, bins, _ = axes[0][1].hist(se, bins=20, label='All points')
+        axes[0][1].hist(se[high_sigma_points],color='r', bins=bins, label=f'{sigma}$\sigma$ points')
+        axes[0][1].axvline(data_mse, color='k', linestyle='dashed', linewidth=1, label="MSE")
+        axes[0][1].set_xlabel('Squared Error (se)')
+        axes[0][1].set_ylabel('Count')
+        axes[0][1].set_title('Histogram of Squared Errors')
+        axes[0][1].set_yscale('log')
+        axes[0][1].legend()
+
+        axes[1][1].axis('off')
+        tbl = axes[1][1].table(cellText=[[f"{data_mse:.2e}"], 
+                                         [f"{max_error:.2e}"],
+                                         [f"{high_sigma_count}"], 
+                                        [f"{max_error_as_a_percent_of_range*100:.2f}%"]],
+                            rowLabels=['MSE', 'Max error',
+                                       f'{sigma}$\sigma$ Count', 'Max error\n% of range'],
+                            loc='center')
+
+
+        
+        tbl.auto_set_font_size(False)
+        tbl.set_fontsize(10)
+        tbl.auto_set_column_width(col=list(range(len(['Value']))))
+        tbl.scale(1.2, 2)
+        tbl.auto_set_font_size(False)
+        tbl.auto_set_column_width([0])
+        axes[1][1].set_title('Summary Statistics')
+
+        plt.tight_layout()
+        if fig_save_path:
+            plt.savefig(fig_save_path)
+        plt.show()
+
+    return data_mse, max_error, max_error_as_a_percent_of_range, data_mse_as_a_percent_of_range
+
+
+def accuracy_results(prooffilename: str, inputfilename: str, settingsfilename: str | None = None, scale: int | None = None, plot_statistical_checks:bool = True, sigma: int = 4, fig_save_path: str | None = None):
+
+    if settingsfilename is not None:
+        with open(settingsfilename, 'r') as f:
+            settings = json.load(f)
+        scale = settings['run_args']['input_scale']
+    elif scale is None:
+        raise ValueError("Must provide either settingsfilename or scale")
+    
+    proof_input, proof_output = proof_file_to_io(prooffilename, scale)
+    input_input, input_output = input_file_to_io(inputfilename)
+    accuracy_analysis(input_input, proof_input, plot_statistical_checks=plot_statistical_checks, sigma=sigma, fig_save_path = fig_save_path + "_input.png" if fig_save_path else None)
+    output_accuracy = accuracy_analysis(input_output, proof_output, plot_statistical_checks=plot_statistical_checks, sigma=sigma, fig_save_path = fig_save_path + "_output.png" if fig_save_path else None)
+    return output_accuracy
