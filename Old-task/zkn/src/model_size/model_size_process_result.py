b50d32626101d0dacf6c043b46ee06bd9cd294c1
Özkan Uysal <61411331+ozkanuysal@users.noreply.github.com>
2024-11-24 19:53:28 +0300

    ref script

--- Old-task/zkn/src/model_size/model_size_process_result.py
+++ Old-task/zkn/src/model_size/model_size_process_result.py
@@ -1,72 +1,110 @@
-import numpy as np, pandas as pd, matplotlib.pyplot as plt, matplotlib as mpl, seaborn as sns
-import math, string, re, pickle, json, os, sys, datetime, itertools
-from collections import Counter
-from tqdm import tqdm
-import glob, re
+import numpy as np
+import pandas as pd
+import glob
+import re
+import json
+from typing import Dict, List, Optional, Tuple
 
-results = []
-for model_type in ['CNN', 'MLP', 'Attn']:
-    files = glob.glob(f'logs/{model_type}*.log')
-    file_dict = {}
-    for file in files:
-        file_number = int(file.split('.')[0].split('_')[-1])
-        file_type = file.split('.')[0].split('_')[-2]
-        if file_number not in file_dict:
-            file_dict[file_number] = {'prework': None, 'setup': None, 'prove': None}
-        file_dict[file_number][file_type] = file
-
-    for number, file_options in file_dict.items():
-        file = file_options['prework']
-        if file is None:
-            print(f"Skipping prework {model_type} {number}")
-            scale, bits, logrows, num_constraints = np.nan, np.nan, np.nan, np.nan
-        else:
-            with open(file) as f:
-                lines = f.readlines()
-            for line in lines:
-                if '- succeeded' in line:
-                    mock_time = float(re.search(r'\d+', line)[0])
-            for line in lines:
-                if '{"run_args":' in line:
-                    settings = json.loads(line.split('[*]')[0])
-                    scale, bits, logrows, num_constraints = settings['run_args']['input_scale'], settings['run_args']['param_scale'], settings['run_args']['logrows'], settings['num_rows']
-            
-        file = file_options['setup']
-        if file is None:
-            print(f"Skipping setup {model_type} {number}")
-            vk_time, pk_time, wall_setup_time = np.nan, np.nan, np.nan
-        else:
-            with open(file) as f:
-                lines = f.readlines()
-            
-            for line in lines:
-                if 'VK took' in line:
-                    vk_time = float(re.search(r'VK took (\d+\.\d+)', line)[1])
-                if 'PK took' in line:
-                    pk_time = float(re.search(r'PK took (\d+\.\d+)', line)[1])
-                if 'succeeded' in line:
-                    wall_setup_time = float(re.search(r'\d+', line)[0])
-
-        file = file_options['prove']
-        if file is None:
-            print(f"Skipping prove {model_type} {number}")
-            proof_time, wall_prove_time = np.nan, np.nan
-        else:
-            with open(file) as f:
-                lines = f.readlines()
-
-            for line in lines:
-                if 'proof took' in line:
-                    proof_time = float(re.search(r'proof took (\d+\.\d+)', line)[1])
-                if 'succeeded' in line:
-                    wall_prove_time = float(re.search(r'\d+', line)[0])
-
-        results.append([model_type, number, scale, bits, logrows, num_constraints, mock_time, vk_time, pk_time, wall_setup_time, proof_time, wall_prove_time])
+class LogFileProcessor:
+    """Process log files for different model types and extract performance metrics."""
     
-results_df = pd.DataFrame(results, columns=['model_type', 'nlayer', 'scale', 'numrows', 'logrows', 'num_constraints', 'mock_time', 'vk_time', 'pk_time', 'wall_setup_time', 'proof_time', 'wall_prove_time'])
+    def __init__(self, base_log_dir: str = 'logs'):
+        self.base_log_dir = base_log_dir
+        self.model_types = ['CNN', 'MLP', 'Attn']
+    
+    def get_files_for_model(self, model_type: str) -> Dict[int, Dict[str, Optional[str]]]:
+        """Organize log files by model number and type."""
+        files = glob.glob(f'{self.base_log_dir}/{model_type}*.log')
+        file_dict = {}
+        
+        for file in files:
+            file_number = int(file.split('.')[0].split('_')[-1])
+            file_type = file.split('.')[0].split('_')[-2]
+            
+            if file_number not in file_dict:
+                file_dict[file_number] = {'prework': None, 'setup': None, 'prove': None}
+            file_dict[file_number][file_type] = file
+            
+        return file_dict
 
+    def process_prework_file(self, file: Optional[str]) -> Tuple[float, float, float, float]:
+        """Extract metrics from prework log file."""
+        if file is None:
+            return np.nan, np.nan, np.nan, np.nan
+        
+        with open(file) as f:
+            lines = f.readlines()
+            
+        mock_time = np.nan
+        scale = bits = logrows = num_constraints = np.nan
+        
+        for line in lines:
+            if '- succeeded' in line:
+                mock_time = float(re.search(r'\d+', line)[0])
+            if '{"run_args":' in line:
+                settings = json.loads(line.split('[*]')[0])
+                scale = settings['run_args']['input_scale']
+                bits = settings['run_args']['param_scale']
+                logrows = settings['run_args']['logrows']
+                num_constraints = settings['num_rows']
+                
+        return scale, bits, logrows, num_constraints
 
-results_python = pd.read_csv('model_size_results_Jan8.csv')
-results_df = results_df.join(results_python.set_index(['modeltype', 'nlayer']), on=['model_type', 'nlayer'])
+    def process_setup_file(self, file: Optional[str]) -> Tuple[float, float, float]:
+        """Extract metrics from setup log file."""
+        if file is None:
+            return np.nan, np.nan, np.nan
+        
+        with open(file) as f:
+            lines = f.readlines()
+            
+        vk_time = pk_time = wall_setup_time = np.nan
+        
+        for line in lines:
+            if 'VK took' in line:
+                vk_time = float(re.search(r'VK took (\d+\.\d+)', line)[1])
+            if 'PK took' in line:
+                pk_time = float(re.search(r'PK took (\d+\.\d+)', line)[1])
+            if 'succeeded' in line:
+                wall_setup_time = float(re.search(r'\d+', line)[0])
+                
+        return vk_time, pk_time, wall_setup_time
 
-results_df.to_csv('model_size_results_compiled_Jan8.csv', index=False)
+    def process_prove_file(self, file: Optional[str]) -> Tuple[float, float]:
+        """Extract metrics from prove log file."""
+        if file is None:
+            return np.nan, np.nan
+        
+        with open(file) as f:
+            lines = f.readlines()
+            
+        proof_time = wall_prove_time = np.nan
+        
+        for line in lines:
+            if 'proof took' in line:
+                proof_time = float(re.search(r'proof took (\d+\.\d+)', line)[1])
+            if 'succeeded' in line:
+                wall_prove_time = float(re.search(r'\d+', line)[0])
+                
+        return proof_time, wall_prove_time
+
+    def process_all_logs(self) -> pd.DataFrame:
+        """Process all log files and compile results into a DataFrame."""
+        results = []
+        
+        for model_type in self.model_types:
+            file_dict = self.get_files_for_model(model_type)
+            
+            for number, file_options in file_dict.items():
+                # Process prework file
+                scale, bits, logrows, num_constraints = self.process_prework_file(file_options['prework'])
+                
+                # Process setup file
+                vk_time, pk_time, wall_setup_time = self.process_setup_file(file_options['setup'])
+                
+                # Process prove file
+                proof_time, wall_prove_time = self.process_prove_file(file_options['prove'])
+                
+                # Compile results
+                results.append([
+                    model_type, number, scale, bits
