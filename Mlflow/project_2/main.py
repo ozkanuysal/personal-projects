6224b105e427b1a318f575b1f935a19088b1686c
Özkan Uysal <61411331+ozkanuysal@users.noreply.github.com>
2024-12-20 11:10:18 +0300

    Refactor main script to use argparse for command-line arguments; rename pipeline function to main for clarity;

--- Mlflow/project_2/main.py
+++ Mlflow/project_2/main.py
@@ -4,34 +4,30 @@ from steps.download_data import load_raw_data
 from steps.preprocess_data import preprocess_data
 from steps.tune_model import tune_model
 from steps.train_final_model import train_model
+import argparse
+import bcolors
 
-class bcolors:
-    HEADER = '\033[95m'
-    OKBLUE = '\033[94m'
-    OKCYAN = '\033[96m'
-    OKGREEN = '\033[92m'
-    WARNING = '\033[93m'
-    FAIL = '\033[91m'
-    ENDC = '\033[0m'
-    BOLD = '\033[1m'
-    UNDERLINE = '\033[4m'
+def parse_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("data_file", type=str)
+    parser.add_argument("n_trials", type=int)
+    return parser.parse_args()
 
-def pipeline():
+def main():
+    args = parse_arguments()
     mlflow.set_experiment("fraud")
-    file_location = load_raw_data(sys.argv[1])
+    file_location = load_raw_data(args.data_file)
     print(f"{bcolors.OKCYAN}Data is loaded{bcolors.ENDC}")
-
     file_dirs = preprocess_data(file_location, missing_thr=0.95)
     print(f"{bcolors.OKCYAN}Data is preprocessed{bcolors.ENDC}")
     best_params = tune_model(
         train_path=file_dirs["train-data-dir"],
         val_path=file_dirs["val-data-dir"],
-        n_trials=int(sys.argv[2]),
+        n_trials=args.n_trials,
     )
     print(f"{bcolors.OKCYAN}HP tuning is finished{bcolors.ENDC}")
     best_params["n_estimators"] = 1000
     best_params["objective"] = "Logloss"
-
     roc, pr = train_model(
         best_params,
         train_path=file_dirs["train-data-dir"],
@@ -41,4 +37,4 @@ def pipeline():
     print(f"{bcolors.OKGREEN}Final model is trained. \nTestset ROC AUC: {roc}\nTestset PR AUC: {pr}{bcolors.ENDC}")
 
 if __name__ == "__main__":
-    pipeline()
+    main()
